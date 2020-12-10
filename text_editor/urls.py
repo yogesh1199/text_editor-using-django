2cb3882518bf1dc28be8e7a05d48fe1dc6169fdc
Yogesh sharma <42976285+yogesh1199@users.noreply.github.com>
2020-12-10 11:11:48 +0530

    Add files via upload

---
+++ text_editor/urls.py
@@ -0,0 +1,31 @@
+"""text_editor URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.contrib import admin
+from.import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    #path('about',views.about,name='about')
+    path('analyzed',views.analyzed,name='analyzed'),
+    #path('capitalize',views.capitalize,name='capitalize'),
+    #path('newline',views.newline,name='newline'),
+    #path('space',views.space,name='space')
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ text_editor/views.py
@@ -0,0 +1,71 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'index.html')
+
+def analyzed(request):
+    djtext=request.POST.get('text','default')
+    djunwant=request.POST.get('unwanted_text','default')
+    removet=request.POST.get('cheak','off')
+    upper=request.POST.get('caps','off')
+    nline=request.POST.get('newline','off')
+    spaceremover=request.POST.get('space','off')
+    word_counter=request.POST.get('count','off')
+    n=len(djtext)-djtext.count(' ')
+    params={}
+    #print(chekbox)
+    #print(djtext)
+    if(removet=='on'):
+        analyzed = ""
+        for char in djtext:
+            if char not in djunwant:
+                analyzed = analyzed + char
+        n=len(analyzed)
+        params = {'purpose': 'remove unwanted text', 'analyze_text': analyzed,'length': n}
+        djtext=analyzed
+        #return render(request,'index.html',params)
+
+    if(upper=='on'):
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        n = len(analyzed)
+        params={'purpose':'convert to capitle','analyze_text': analyzed,'length': n}
+
+        djtext=analyzed
+        #return render(request,'index.html',params)
+
+    if(nline=='on'):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n":
+                analyzed = analyzed + char
+        n = len(analyzed)
+        #print("pre", analyzed)
+        params = {'purpose': 'Removed NewLines', 'analyze_text': analyzed,'length': n}
+        djtext = analyzed
+        #print(params)
+        # Analyze the text
+        #return render(request, 'index.html', params)
+
+    if(spaceremover=='on'):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not (djtext[index] == " " and djtext[index + 1] == " "):
+                analyzed = analyzed + char
+
+            n = len(analyzed)
+
+        params = {'purpose': 'Removed NewLines' ,'analyze_text': analyzed,'length': n}
+        djtext=analyzed
+        # Analyze the text
+        #return render(request, 'index.html', params)
+    if(removet!='on' and upper!='on' and nline=='on' and spaceremover!='on'):
+        n = len(djtext)
+        params = {'purpose': 'please select option and try again', 'analyze_text': 'error','length':n}
+        #return render(request, 'index.html', params)
+    #else:
+     #   params = {'purpose': 'please seslect option', 'analyze_text': 'error'}
+
+    return render(request, 'index.html', params)
